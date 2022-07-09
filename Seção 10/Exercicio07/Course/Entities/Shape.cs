b74b9fcc6482cf8be7e1baa09c2ba6ae47de4cef
FabioNunesDEV <fabio.nunes@silo.com.vc>
2022-07-09 16:24:53 -0300

    Adicionado exercicio de fixação seção 11

--- Seção 10/Exercicio07/Course/Entities/Shape.cs
+++ Seção 10/Exercicio07/Course/Entities/Shape.cs
@@ -5,12 +5,10 @@ namespace Course.Entities
     abstract class Shape
     {
         public Color Color { get; set; }
-
         public Shape(Color color)
         {
             Color = color;
         }
-
         public abstract double Area();
     }
 }
---
+++ Seção 11/Exercicio01/Course/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite dois números para serem divididos:");
+                Console.Write("Nº1: ");
+                int n1 = int.Parse(Console.ReadLine());
+                Console.Write("Nº2: ");
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+
+                Console.WriteLine("");
+                Console.WriteLine("O resultado é: " + result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divisão por zero não é possivel.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Digite apenas números.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
