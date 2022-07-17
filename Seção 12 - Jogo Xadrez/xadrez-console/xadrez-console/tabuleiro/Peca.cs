3847eaab2a92b520969e0db94120bd1aefae557a
FabioNunesDEV <fabio.nunes@silo.com.vc>
2022-07-17 15:18:37 -0300

    Comencando a implemetar o tabuleiro

---
+++ Seção 12 - Jogo Xadrez/xadrez-console/xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,19 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posiao { get; set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+        public Cor cor { get; protected set; }
+        public int gteMovimentos { get; protected set; }
+
+        public Peca(Posicao posiao, Tabuleiro tabuleiro, Cor cor)
+        {
+            this.posiao = posiao;
+            this.tabuleiro = tabuleiro;
+            this.cor = cor;
+            this.gteMovimentos = 0;
+        }
+    }
+
+}
