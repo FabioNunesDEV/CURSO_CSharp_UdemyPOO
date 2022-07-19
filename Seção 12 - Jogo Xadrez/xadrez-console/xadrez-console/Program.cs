fdb16705704543035897fbeb9f85cfcaca71212b
FabioNunesDEV <fabio.nunes@silo.com.vc>
2022-07-18 21:06:29 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- Seção 12 - Jogo Xadrez/xadrez-console/xadrez-console/Program.cs
+++ Seção 12 - Jogo Xadrez/xadrez-console/xadrez-console/Program.cs
@@ -13,8 +13,12 @@ namespace xadrez_console
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 6));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(4, 5));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(6, 7));
 
                 Tela.imprimirTabuleiro(tab);
             }
