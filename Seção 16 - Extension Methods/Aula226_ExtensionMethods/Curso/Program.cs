1bef78b2793fc3bb97f387d5653b603d8a470741
FabioNunesDEV <fabionunesbr@gmail.com>
2022-08-18 08:51:06 -0300

    Adicionada secao 16

---
+++ Seção 16 - Extension Methods/Aula226_ExtensionMethods/Curso/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime datePost = new DateTime(2022, 08, 18, 07, 40, 00);
+            Console.WriteLine("Time elapsed: " + datePost.ElapsedTime());
+
+            string txt = "Good morning dear stundents !";
+            Console.WriteLine("Resume: " + txt.Cut(10));
+
+            Console.ReadLine();
+        }
+    }
+}
